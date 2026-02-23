91701e5b6f846c3b3bd5460a6bd0e12dbbd5ea59
hungkjkj <122081586+hungkjkj@users.noreply.github.com>
2026-02-23 16:42:25 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const ccxt = require('ccxt');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
